788572a299a8df9c72989ff9147974331d938ee3
Kirill Gribanov <u10110.uu@gmail.com>
2026-08-18 09:37:21 +0300

    Keep about page visible in public menu

--- src/lib/admin-menu.test.js
+++ src/lib/admin-menu.test.js
@@ -5,5 +5,5 @@ import { readFile } from 'node:fs/promises';
 test('static menu links are rendered only for administrators', async () => {
   const appSource = await readFile(new URL('../App.vue', import.meta.url), 'utf8');
   assert.match(appSource, /const isAdmin = computed\(\(\) => currentUser\.value\?\.role === 'admin'\);/);
-  assert.match(appSource, /<template v-if="isAdmin">[\s\S]*?to="\/promotion"[\s\S]*?to="\/about"[\s\S]*?<\/template>/);
+  assert.match(appSource, /<template v-if="isAdmin">[\s\S]*?to="\/promotion"[\s\S]*?to="\/blogs"[\s\S]*?<\/template>\s*<RouterLink to="\/about">/);
 });
